280d4022b8c403daffd85a0532e978cd08e19085
RyoTa <ryotanny@gmail.com>
2024-09-01 10:04:51 +0900

    first commit

---
+++ src/stories/Page.stories.ts
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+import { within, userEvent, expect, waitFor } from '@storybook/test';
+
+import Page from './Page.svelte';
+
+const meta = {
+	title: 'Example/Page',
+	component: Page,
+	parameters: {
+		// More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+		layout: 'fullscreen'
+	}
+} satisfies Meta<Page>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoggedOut: Story = {
+	parameters: {
+		a11y: {
+			// This option disables all a11y checks on this story
+			disable: true
+		}
+	}
+};
+
+// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
+export const LoggedIn: Story = {
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const loginButton = canvas.getByRole('button', { name: /Log in/i });
+		await expect(loginButton).toBeInTheDocument();
+		await userEvent.click(loginButton);
+		await waitFor(() => expect(loginButton).not.toBeInTheDocument());
+
+		const logoutButton = canvas.getByRole('button', { name: /Log out/i });
+		await expect(logoutButton).toBeInTheDocument();
+	},
+	parameters: {
+		a11y: {
+			// This option disables all a11y checks on this story
+			disable: true
+		}
+	}
+};
