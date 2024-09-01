e95a6709dc8cd4bc7cadce6f5d532b9daef312c6
RyoTa <ryotanny@gmail.com>
2024-09-01 11:20:53 +0900

    feat: Update test-runner

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -10,19 +10,23 @@ const config: TestRunnerConfig = {
 		await injectAxe(page);
 	},
 	async postVisit(page, context) {
+		// snapshot test
+		const elementHandler = await page.$('#storybook-root');
+		if (elementHandler) {
+			const innerHTML = await elementHandler.innerHTML();
+			expect(innerHTML).toMatchSnapshot();
+		}
+		// a11y test
 		// Get the entire context of a story, including parameters, args, argTypes, etc.
 		const storyContext = await getStoryContext(page, context);
-
 		// Do not run a11y tests on disabled stories.
 		if (storyContext.parameters?.a11y?.disable) {
 			return;
 		}
-
 		// Apply story-level a11y rules
 		await configureAxe(page, {
 			rules: storyContext.parameters?.a11y?.config?.rules
 		});
-
 		await checkA11y(page, '#storybook-root', {
 			detailedReport: true,
 			detailedReportOptions: {
