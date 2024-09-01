fdab7a6ab5705cfee31e151ecfa8a6a03448215d
RyoTa <ryotanny@gmail.com>
2024-09-01 10:25:42 +0900

    feat: Update page style

--- src/stories/Page.svelte
+++ src/stories/Page.svelte
@@ -16,17 +16,6 @@
 	<section class="storybook-page">
 		<h2>Pages in Storybook</h2>
 		<p>
-			We recommend building UIs with a
-			<a
-				href="https://blog.hichroma.com/component-driven-development-ce1109d56c8e"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<strong>component-driven</strong>
-			</a>
-			process starting with atomic components and ending with pages.
-		</p>
-		<p>
 			Render pages with mock data. This makes it easy to build and review page states without
 			needing to navigate to them in your app. Here are some handy patterns for managing page data
 			in Storybook:
@@ -41,15 +30,6 @@
 				using Storybook.
 			</li>
 		</ul>
-		<p>
-			Get a guided tutorial on component-driven development at
-			<a href="https://storybook.js.org/tutorials/" target="_blank" rel="noopener noreferrer">
-				Storybook tutorials
-			</a>
-			. Read more in the
-			<a href="https://storybook.js.org/docs" target="_blank" rel="noopener noreferrer">docs</a>
-			.
-		</p>
 		<div class="tip-wrapper">
 			<span class="tip">Tip</span>
 			Adjust the width of the canvas with the
