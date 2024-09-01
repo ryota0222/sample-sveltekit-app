fd7d90a6bdca14217d89c9069351084cadcd64b6
RyoTa <ryotanny@gmail.com>
2024-09-01 16:02:43 +0900

    feat: Add unit test to Button

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,18 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/svelte';
 import Button from './Button.svelte';
+import { within, expect } from '@storybook/test';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-  title: 'Example/Button',
-  component: Button,
-  tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    size: {
-      control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
-    },
-  },
+	title: 'Example/Button',
+	component: Button,
+	tags: ['autodocs'],
+	argTypes: {
+		backgroundColor: { control: 'color' },
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large']
+		}
+	}
 } satisfies Meta<Button>;
 
 export default meta;
@@ -20,28 +21,34 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
+	args: {
+		primary: true,
+		label: 'Button',
+		disabled: true
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const button = canvas.getByRole('button', { name: /Button/i });
+		expect(button.getAttribute('disabled')).toBe('');
+	}
 };
 
 export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
+	args: {
+		label: 'Button'
+	}
 };
 
 export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
+	args: {
+		size: 'large',
+		label: 'Button'
+	}
 };
 
 export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
+	args: {
+		size: 'small',
+		label: 'Button'
+	}
 };
